0a9477083c58c53a6e553058575894490dcb3d18
Emanoel <emanoel.pluto@gmail.com>
2026-06-24 13:55:38 -0300

    editing password and user fields

--- src/br/com/company/domain/ConnectionFactory.java
+++ src/br/com/company/domain/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection recoverConnection() throws SQLException {
 		return DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useTimezone=true&serverTimezone=UTC",
-				"root" , "82551210Eh.");
+				"user" , "password");
 		
 //		return DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root");
 	}
--- src/br/com/company/test/ConnectionTest.java
+++ src/br/com/company/test/ConnectionTest.java
@@ -8,7 +8,7 @@ public class ConnectionTest {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useTimezone=true&serverTimezone=UTC",
-				"root" , "82551210Eh.");
+				"user" , "password");
 		
 		System.out.println("It Worked!!!");
 		
